7217d041ffefe214ce4761d93401d59bfff15aa4
Ariadna Zeballos <zeballosariadna973@gmail.com>
2022-04-22 12:39:23 -0300

    Creando el Sistema de Registro y Login - Generando los JWTs

---
+++ PeliculasAPI/Controllers/CuentasController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using PeliculasAPI.DTOs;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace PeliculasAPI.Controllers
+{
+    [Route("api/cuentas")]
+    [ApiController]
+    public class CuentasController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+        private readonly IConfiguration configuration;
+
+        public CuentasController(UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager,
+            IConfiguration configuration)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.configuration = configuration;
+        }
+
+        [HttpPost("crear")]
+        public async Task<ActionResult<RespuestaAutenticacion>> Crear([FromBody] CredencialesUsuario credenciales)
+        {//IdentityUser: obj que identifica un usuario en la app
+            var usuario = new IdentityUser { UserName = credenciales.Email, Email = credenciales.Email };
+            var resultado = await userManager.CreateAsync(usuario, credenciales.Password);
+
+            if (resultado.Succeeded)//retorna token
+            {
+                return await ConstruirToken(credenciales);
+            }
+            else
+            {
+                return BadRequest(resultado.Errors);
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<RespuestaAutenticacion>> Login([FromBody] CredencialesUsuario credenciales)
+        {
+            var resultado = await signInManager.PasswordSignInAsync(credenciales.Email, credenciales.Password,
+                isPersistent: false, lockoutOnFailure: false);
+
+            if (resultado.Succeeded)
+            {
+                return await ConstruirToken(credenciales);
+            }
+            else
+            {
+                return BadRequest("Login incorrecto");
+            }
+        }
+
+        private async Task<RespuestaAutenticacion> ConstruirToken(CredencialesUsuario credenciales)
+        {//conj de claims que colocaremos en JSONWebToken
+            var claims = new List<Claim>()
+            {
+                new Claim("email",credenciales.Email)
+            };
+
+            var usuario = await userManager.FindByEmailAsync(credenciales.Email);
+            var claimsDB = await userManager.GetClaimsAsync(usuario);//Listado de claims de la BD del usuario correspondiente
+
+            claims.AddRange(claimsDB);
+
+            //construccion del JSONWebToken
+            var llave = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["llavejwt"]));
+            var creds = new SigningCredentials(llave, SecurityAlgorithms.HmacSha256);
+
+            var expiracion = DateTime.UtcNow.AddYears(1);//expiracion del token
+
+            var token = new JwtSecurityToken(issuer: null, audience: null, claims: claims,
+                    expires: expiracion, signingCredentials: creds);
+
+            return new RespuestaAutenticacion
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                Expiracion = expiracion
+            };
+        }
+
+    }
+}
